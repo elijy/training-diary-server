b241e9b41fc6ace5caae4fa3c409d4156617143f
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-02-17 20:40:20 -0500

    Set up get workouts route

--- routes/workouts.js
+++ routes/workouts.js
@@ -2,8 +2,23 @@ const express = require("express");
 
 const router = express.Router();
 
+const WORKOUTS = [
+  {
+    id: "40bf",
+    date: "2024-02-04T19:51:04.463Z",
+  },
+  {
+    id: "ab64",
+    date: "2024-02-04T19:55:34.707Z",
+  },
+  {
+    id: "38a3",
+    date: "2024-02-08T00:13:03.931Z",
+  },
+];
+
 router.get("/workouts", async (req, res) => {
-  res.send("Hello World!");
+  res.send(WORKOUTS);
 });
 
 module.exports = router;
