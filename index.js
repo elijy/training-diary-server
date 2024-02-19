a47a6fdccfd1ae0528befa17853bd5a4af692024
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-02-18 19:16:43 -0500

    add body parser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const workoutRouter = require("./routes/workouts");
 
 const app = express();
 const port = 3005;
 
+app.use(bodyParser.json());
 app.use(workoutRouter);
 
 app.listen(port, () => {
