5299307339e2a19d25ce9acc7474bb796a1bc8f1
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-02-23 18:04:51 -0500

    post sets

--- routes/sets.js
+++ routes/sets.js
@@ -11,4 +11,14 @@ router.get("/sets", async (req, res) => {
   res.send(data);
 });
 
+router.post("/sets", async (req, res) => {
+  const { exerciseId, weight, reps } = req.body;
+  const { data } = await axios.post("http://localhost:3001/sets", {
+    exerciseId,
+    weight,
+    reps,
+  });
+  res.send(data);
+});
+
 module.exports = router;
