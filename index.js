01cc94ac7f3f13b703f71dd9674a09b43e4febe5
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-03-16 16:54:25 -0400

    connect to postgres

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { graphqlHTTP as expressGraphQL } from "express-graphql";
+import pg from "pg";
 
 import schema from "./schema/schema.js";
 
@@ -11,6 +12,14 @@ import setsRouter from "./routes/sets.js";
 
 const app = express();
 const port = 3005;
+
+const client = new pg.Client({
+  host: "localhost",
+  port: 5432,
+  database: "training-diary",
+  user: "elijy",
+  password: "",
+});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -25,6 +34,8 @@ app.use(
     graphiql: true,
   })
 );
+
+await client.connect();
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
