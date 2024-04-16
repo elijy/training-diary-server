34f2012141ad8f30580c7b68c4c374a528aa6f12
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-04-16 19:05:56 -0400

    remove unused router

--- index.js
+++ index.js
@@ -6,18 +6,11 @@ import { client } from "./db/index.js";
 
 import schema from "./schema/schema.js";
 
-import workoutRouter from "./routes/workouts.js";
-import exercisesRouter from "./routes/exercises.js";
-import setsRouter from "./routes/sets.js";
-
 const app = express();
 const port = 3005;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(workoutRouter);
-app.use(exercisesRouter);
-app.use(setsRouter);
 
 app.use(
   "/graphql",
